d619d620cfc8c6d28c81088203e032dadad73653
Ignace De Cock <ignace.de.cock@cloudasc.net>
2024-05-18 00:33:13 +0200

    Update jupyter

--- docker/helk-jupyter/scripts/jupyter-entrypoint.sh
+++ docker/helk-jupyter/scripts/jupyter-entrypoint.sh
@@ -14,10 +14,10 @@ NOTEBOOK_ERROR_TAG="[NOTEBOOK-JUPYTER-DOCKER-INSTALLATION-ERROR]"
 PGDATA=${PGDATA:-/home/jupyter/srv/pgsql}
  
 if [ ! -d "$PGDATA" ]; then
-  /usr/lib/postgresql/10/bin/initdb -D "$PGDATA" --auth-host=md5 --encoding=UTF8
+  /usr/lib/postgresql/16/bin/initdb -D "$PGDATA" --auth-host=md5 --encoding=UTF8
 fi
 echo "$NOTEBOOK_INFO_TAG The files in this database system will be owned by user jupyter.."
-/usr/lib/postgresql/10/bin/pg_ctl -D "$PGDATA" status || /usr/lib/postgresql/10/bin/pg_ctl -D "$PGDATA" -l "$PGDATA/pg.log" start
+/usr/lib/postgresql/16/bin/pg_ctl -D "$PGDATA" status || /usr/lib/postgresql/16/bin/pg_ctl -D "$PGDATA" -l "$PGDATA/pg.log" start
 
 # ************ Checking if user hive exists ****************
 echo "$NOTEBOOK_INFO_TAG Checking if user hive already exists.."
